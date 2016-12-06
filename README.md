90e5279354c2f83b5874098e53d1f20cd5538f7d
BradM <bradm@inmotionhosting.com>
2016-12-06 09:10:56 -0500

    revert 751fd54

--- README.md
+++ README.md
@@ -28,7 +28,6 @@ BoldGrid Staging is a standalone plugin to allow use of a staged website while k
 ## Changelog ##
 
 ### 1.3.3 In progress ###
-* Bug fix:		JIRA WPB-2635	Start over staging affecting active site.
 * Bug fix:		JIRA WPB-2664	Memory error when loading Customizer.
 
 ### 1.3.2 ###
--- includes/class-boldgrid-staging-theme.php
+++ includes/class-boldgrid-staging-theme.php
@@ -142,10 +142,6 @@ class Boldgrid_Staging_Theme extends Boldgrid_Staging_Base {
 
 			add_filter( 'wp_prepare_themes_for_js', array( $this, 'filter_wp_prepare_themes_for_js' ) );
 
-			add_filter( 'get_staging_theme_mod', array( $this, 'get_staging_theme_mod' ), 10, 2 );
-
-			add_action( 'set_staging_theme_mod', array( $this, 'set_staging_theme_mod' ), 10, 2 );
-
 			add_action( 'admin_enqueue_scripts', array( $this, 'customizer_switch_theme' ) );
 		}
 
@@ -254,21 +250,6 @@ a.button.button-primary.customize.load-customize.hide-if-no-customize {
 		}
 
 		wp_die();
-	}
-
-	/**
-	 * Set a staged theme mod.
-	 *
-	 * This method does not rely on the standard ?staging=1.
-	 *
-	 * @since 1.3.3
-	 */
-	public function set_staging_theme_mod( $mod, $value ) {
-		$mods = $this->get_staging_theme_mods();
-
-		$mods[$mod] = $value;
-
-		update_option( 'boldgrid_staging_theme_mods_' . $this->staging_stylesheet, $mods );
 	}
 
 	/**
@@ -382,32 +363,6 @@ a.button.button-primary.customize.load-customize.hide-if-no-customize {
 		}
 
 		return $new_value;
-	}
-
-	/**
-	 * Get a staging theme mod.
-	 *
-	 * @since 1.3.3
-	 *
-	 * @param  string $name    The theme mod name.
-	 * @param  mixed  $default The default value to return.
-	 * @return mixed
-	 */
-	public function get_staging_theme_mod( $name, $default ) {
-		$mods = $this->get_staging_theme_mods();
-
-		return ( ! empty( $mods[$name] ) ? $mods[$name] : $default );
-	}
-
-	/**
-	 * Get the staging theme's mods.
-	 *
-	 * This method does not rely on the standard ?staging=1.
-	 *
-	 * @since 1.3.3
-	 */
-	public function get_staging_theme_mods() {
-		return get_option( 'boldgrid_staging_theme_mods_' . $this->staging_stylesheet, array() );
 	}
 
 	/**
